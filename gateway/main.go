9bf2686f02b2799e96e06e2550df240ffa011aca
lazar.stojcevic <stojcevic00@gmail.com>
2022-06-14 20:07:28 +0200

    Little refactor

--- gateway/main.go
+++ gateway/main.go
@@ -17,14 +17,6 @@ func main() {
 	log.Out = os.Stdout
 
 	path := "gateway.log"
-	/*Log rotation correlation function
-	`Withlinkname 'establishes a soft connection for the latest logs
-	`Withrotationtime 'sets the time of log splitting, and how often to split
-	Only one of withmaxage and withrotationcount can be set
-	 `Withmaxage 'sets the maximum save time before cleaning the file
-	 `Withrotationcount 'sets the maximum number of files to be saved before cleaning
-	*/
-	//The following configuration logs rotate a new file every 1 minute, keep the log files of the last 3 minutes, and automatically clean up the surplus.
 	writer, err := rotatelogs.New(
 		path+".%Y%m%d%H%M",
 		rotatelogs.WithLinkName(path),
